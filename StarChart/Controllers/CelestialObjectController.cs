55934978b894ce1bfe8a2719c4842fedfc31ee18
Eric J Fisher <eric-fisher@pluralsight.com>
2018-08-15 12:04:16 -0400

    Removed Module 4 Code

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -13,45 +12,6 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var celestialObject = _context.CelestialObjects.Find(id);
-            if (celestialObject == null)
-                return NotFound();
-
-            celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObject.Id == celestialObject.Id).ToList();
-
-            return Ok(celestialObject);
-        }
-
-        [HttpGet("{name}")]
-        public IActionResult GetByName(string name)
-        {
-            var celestialObjects = _context.CelestialObjects.Where(e => e.Name == name);
-            if (celestialObjects == null)
-                return NotFound();
-
-            foreach(var item in celestialObjects)
-            {
-                item.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObject.Id == item.Id).ToList();
-            }
-
-            return Ok(_context.CelestialObjects.Where(e => e.Name == name));
-        }
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var celestialObjects = _context.CelestialObjects.ToList();
-            foreach (var item in celestialObjects)
-            {
-                item.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObject.Id == item.Id).ToList();
-            }
-
-            return Ok(celestialObjects);
         }
     }
 }

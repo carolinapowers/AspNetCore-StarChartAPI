1ab009198f691e256f0fcae280941e57290ee86b
Eric J Fisher <eric-fisher@pluralsight.com>
2018-09-20 11:39:31 -0400

    Updated Solution to fit new instructions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -67,7 +67,7 @@ namespace StarChart.Controllers
                 return NotFound();
             existingObject.Name = celestialObject.Name;
             existingObject.OrbitalPeriod = celestialObject.OrbitalPeriod;
-            existingObject.OrbitedObject = celestialObject.OrbitedObject;
+            existingObject.OrbitedObjectId = celestialObject.OrbitedObjectId;
             _context.CelestialObjects.Update(existingObject);
             _context.SaveChanges();
             return NoContent();
@@ -88,7 +88,7 @@ namespace StarChart.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObject.Id == id);
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
             if (!celestialObjects.Any())
                 return NotFound();
             _context.CelestialObjects.RemoveRange(celestialObjects);
